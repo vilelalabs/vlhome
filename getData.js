5f56bfd0a26e36d79dce797ecac508f452848585
hvilela0 <hvilela0@gmail.com>
2022-04-07 09:42:29 -0300

    Install first devices working.

--- getData.js
+++ getData.js
@@ -1,16 +1,26 @@
 import Factory from './src/logic_components/Factory';
 
 import RNFetchBlob from 'react-native-fetch-blob';
-let data = require('./src/logic_components/initData.json');
+//let initData = require('./src/logic_components/initData.json');
+let initData = require('./positions.json');
 //import data from './positions.json'
-
+let data = initData;
 async function getData() {
     let allAmbients = [];
     //const response = await axios.get('https://testeesp8266-ef2ce-default-rtdb.firebaseio.com/.json');
 
     //read data with RNFetchBlob
     const pathToRead = `${RNFetchBlob.fs.dirs.DocumentDir}/positions.json`;
-    data = JSON.parse(await RNFetchBlob.fs.readFile(pathToRead, 'utf8'));
+    try {
+        data = JSON.parse(await RNFetchBlob.fs.readFile(pathToRead, 'utf8'));
+        console.log('Arquivo lido com sucesso!')
+    } catch (error) {
+        console.log(error);
+        console.log('File not found');
+    }
+
+
+
     if (data) {
         data.ambients.forEach(amb => {
             allAmbients.push(Factory('ambient', amb.id, amb.iconName, amb.name, amb.devices, amb.order));
@@ -18,9 +28,22 @@ async function getData() {
         return allAmbients;
     }
     else {
+        initData.ambients.forEach(amb => {
+            allAmbients.push(Factory('ambient', amb.id, amb.iconName, amb.name, amb.devices, amb.order));
+        });
         console.log('no data');
         return allAmbients;
     }
 }
 
 export default getData;
+
+/*
+ const pathToWrite = `${RNFetchBlob.fs.dirs.DocumentDir}/positions.json`;
+
+            const JSONfile = "{\"ambients\": " + JSON.stringify(allAmbients) + "}";
+            RNFetchBlob.fs
+                .writeFile(pathToWrite, JSONfile, 'utf8')
+                .then(() => { })
+                .catch(error => console.error(error));
+*/
